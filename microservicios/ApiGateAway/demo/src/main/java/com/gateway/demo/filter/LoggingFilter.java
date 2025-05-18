e01b88bdfec3176e935823bae98dc491c248f334
alonsodm <alonsodmx@gmail.com>
2025-05-18 18:54:32 +0200

    fix(develop) : Arreglo github actions para primera iteracion

--- microservicios/ApiGateAway/demo/src/main/java/com/gateway/demo/filter/LoggingFilter.java
+++ microservicios/ApiGateAway/demo/src/main/java/com/gateway/demo/filter/LoggingFilter.java
@@ -1,6 +1,5 @@
 package com.gateway.demo.filter;
 
-import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.stereotype.Component;
